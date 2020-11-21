9550ae6f28fc68e863f77655f773d91c68f291e0
Sambo Chea <sombochea@cubetiqs.com>
2020-11-21 10:44:30 +0700

    init the example modules

---
+++ src/main/kotlin/com/cubetiqs/modules/example/SampleModuleExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.cubetiqs.modules.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleModuleExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SampleModuleExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/cubetiqs/modules/example/SampleModuleExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cubetiqs.modules.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleModuleExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
